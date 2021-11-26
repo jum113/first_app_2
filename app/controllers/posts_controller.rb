69118ea7789a9d55a17c7e378653bf0d97642654
jum113 <japan_movieact@yahoo.co.jp>
2021-11-26 22:01:35 +0900

    再実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  def index
+    @post = Post.all
+    @post.each do |post|
+      post.content
+    end
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
 end
